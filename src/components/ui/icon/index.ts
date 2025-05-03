d51ee2dbd97499db26dcde0a7cad8eb1218eeea5
Silinator <me@silvanfux.ch>
2025-05-04 00:19:15 +0200

    Adjusted xl icon size.

--- src/components/ui/icon/index.ts
+++ src/components/ui/icon/index.ts
@@ -3,24 +3,24 @@ import { cva, type VariantProps } from 'class-variance-authority';
 export { default as Icon } from './Icon.vue';
 
 export const iconVariants = cva('pointer-events-none select-none', {
-    variants: {
-        variant: {
-            outlined: 'mui-symbols mui-symbols-outlined',
-            rounded: 'mui-symbols mui-symbols-rounded',
-            sharp: 'mui-symbols mui-symbols-sharp',
-        },
-        size: {
-            xs: 'text-lg',
-            sm: 'text-2xl',
-            md: 'text-3xl',
-            lg: 'text-4xl',
-            xl: 'text-5xl',
-        },
+  variants: {
+    variant: {
+      outlined: 'mui-symbols mui-symbols-outlined',
+      rounded: 'mui-symbols mui-symbols-rounded',
+      sharp: 'mui-symbols mui-symbols-sharp',
     },
-    defaultVariants: {
-        variant: 'rounded',
-        size: 'md',
+    size: {
+      xs: 'text-lg',
+      sm: 'text-2xl',
+      md: 'text-3xl',
+      lg: 'text-4xl',
+      xl: 'text-[42px]',
     },
+  },
+  defaultVariants: {
+    variant: 'rounded',
+    size: 'md',
+  },
 });
 
 export type IconVariants = VariantProps<typeof iconVariants>;
