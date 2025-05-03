703d960cabf104fe548a30829bd43d0b11a846b9
Silinator <me@silvanfux.ch>
2025-05-03 21:09:37 +0200

    Added vueDevTools, prettier and eslint.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss(), vueDevTools()],
 });
