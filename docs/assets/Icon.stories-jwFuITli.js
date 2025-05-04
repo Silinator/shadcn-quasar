b1860f5e7250c1d7e8ed4c0c4484e7ef3ee0a0b7
Silinator <me@silvanfux.ch>
2025-05-05 00:59:05 +0200

    Added docs for github pages.

---
+++ docs/assets/Icon.stories-jwFuITli.js
@@ -0,0 +1,30 @@
+import{I}from"./Icon-BVv79Dgq.js";import"./vue.esm-bundler-D4xhGFi4.js";const z={title:"UI/Icon",component:I,tags:["autodocs"]},a={args:{name:"font_download"}},o={args:{name:"font_download",class:"text-blue-500"}},e={args:{name:"font_download",filled:!0}},r={args:{name:"font_download",variant:"outlined"}},s={args:{name:"font_download",variant:"sharp"}},n={args:{name:"font_download",class:"text-8xl"}};var t,c,d;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download'
+  }
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,l,i;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download',
+    class: 'text-blue-500'
+  }
+}`,...(i=(l=o.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};var p,u,g;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download',
+    filled: true
+  }
+}`,...(g=(u=e.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var f,_,w;r.parameters={...r.parameters,docs:{...(f=r.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download',
+    variant: 'outlined'
+  }
+}`,...(w=(_=r.parameters)==null?void 0:_.docs)==null?void 0:w.source}}};var S,x,C;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download',
+    variant: 'sharp'
+  }
+}`,...(C=(x=s.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var h,v,B;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    name: 'font_download',
+    class: 'text-8xl'
+  }
+}`,...(B=(v=n.parameters)==null?void 0:v.docs)==null?void 0:B.source}}};const F=["Basic","BasicCustomColors","Filled","Outlined","Sharp","CustomSize"];export{a as Basic,o as BasicCustomColors,n as CustomSize,e as Filled,r as Outlined,s as Sharp,F as __namedExportsOrder,z as default};
