2a8af4ecb68648935ea41810ddc86b59352167d0
Silinator <me@silvanfux.ch>
2025-06-09 00:48:11 +0200

    Build storybook.

---
+++ docs/assets/Badge.stories-C1E_evlp.js
@@ -0,0 +1,254 @@
+import{_ as n}from"./Badge-muRYF2Ur.js";import{_ as i}from"./Button-Bd1hwruy.js";import{I as m}from"./Icon-BzsAYEaQ.js";import"./iframe-Dih_2Swd.js";import"./_plugin-vue_export-helper-CqjgSE-H.js";import"./Primitive-CB9pkJf-.js";import"./ripple-CsoxYJ_M.js";const ne={title:"UI/Badge",component:n,tags:["autodocs"]},a={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <Badge v-bind="args">${e.default}</Badge>
+    `}),args:{default:"#007ABF"}},t={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <Badge v-bind="args">${e.default}</Badge>
+    `}),args:{class:"bg-orange-400 text-red-800",default:"7"}},r={render:e=>({components:{Badge:n,Icon:m},setup(){return{args:e}},template:`
+      <Badge v-bind="args">${e.default}</Badge>
+    `}),args:{class:"bg-purple-400",default:'<Icon name="bluetooth" class="text-primary-foreground" />'}},s={render:e=>({components:{Badge:n,Icon:m,Button:i},setup(){return{args:e}},template:`
+      <Button> Button <Badge v-bind="args">${e.default}</Badge> </Button> 
+    `}),args:{class:"bg-cyan-600 text-white",floating:!0,default:"22"}},o={render:e=>({components:{Badge:n,Icon:m,Button:i},setup(){return{args:e}},template:`
+      <Button round> <Icon name="notifications" size="sm" filled class="text-primary-foreground" /> <Badge v-bind="args">${e.default}</Badge> </Button> 
+    `}),args:{class:"bg-pink-200 text-black",floating:!0,default:""}},d={render:e=>({components:{Badge:n,Icon:m,Button:i},setup(){return{args:e}},template:`
+      <Button> Button <Badge v-bind="args">${e.default}</Badge> </Button> 
+    `}),args:{class:"bg-yellow-600 text-white",floating:!0,transparent:!0,default:"22"}},g={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <div class="text-3xl">
+        Text
+        <Badge v-bind="args">${e.default}</Badge>
+      </div>
+    `}),args:{class:"bg-blue-600",align:"top",default:"app v1.8.7"}},u={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <div class="text-3xl">
+        Text
+        <Badge v-bind="args">${e.default}</Badge>
+      </div>
+    `}),args:{class:"bg-orange-600",transparent:!0,default:"app v1.8.7"}},c={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <Badge v-bind="args">${e.default}</Badge>
+    `}),args:{class:"text-blue-600",outline:!0,default:"Outline"}},l={render:e=>({components:{Badge:n},setup(){return{args:e}},template:`
+      <Badge v-bind="args">${e.default}</Badge>
+    `}),args:{class:"bg-red-600",rounded:!0,default:"Round"}},p={render:e=>({components:{Badge:n,Button:i},setup(){return{args:e}},template:`
+      <div class="flex items-center gap-2">
+        Status
+        <Badge v-bind="args">${e.default}</Badge>
+      </div>
+    `}),args:{class:"bg-green-400",default:""}};var B,f,b;a.parameters={...a.parameters,docs:{...(B=a.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Badge v-bind="args">\${args.default}</Badge>
+    \`
+  }),
+  args: {
+    default: \`#007ABF\`
+  }
+}`,...(b=(f=a.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var v,x,$;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Badge v-bind="args">\${args.default}</Badge>
+    \`
+  }),
+  args: {
+    class: 'bg-orange-400 text-red-800',
+    default: \`7\`
+  }
+}`,...($=(x=t.parameters)==null?void 0:x.docs)==null?void 0:$.source}}};var I,S,h;r.parameters={...r.parameters,docs:{...(I=r.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Badge v-bind="args">\${args.default}</Badge>
+    \`
+  }),
+  args: {
+    class: 'bg-purple-400',
+    default: \`<Icon name="bluetooth" class="text-primary-foreground" />\`
+  }
+}`,...(h=(S=r.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};var y,T,_;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge,
+      Icon,
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button> Button <Badge v-bind="args">\${args.default}</Badge> </Button> 
+    \`
+  }),
+  args: {
+    class: 'bg-cyan-600 text-white',
+    floating: true,
+    default: \`22\`
+  }
+}`,...(_=(T=s.parameters)==null?void 0:T.docs)==null?void 0:_.source}}};var w,F,O;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge,
+      Icon,
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button round> <Icon name="notifications" size="sm" filled class="text-primary-foreground" /> <Badge v-bind="args">\${args.default}</Badge> </Button> 
+    \`
+  }),
+  args: {
+    class: 'bg-pink-200 text-black',
+    floating: true,
+    default: \`\`
+  }
+}`,...(O=(F=o.parameters)==null?void 0:F.docs)==null?void 0:O.source}}};var k,A,C;d.parameters={...d.parameters,docs:{...(k=d.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge,
+      Icon,
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button> Button <Badge v-bind="args">\${args.default}</Badge> </Button> 
+    \`
+  }),
+  args: {
+    class: 'bg-yellow-600 text-white',
+    floating: true,
+    transparent: true,
+    default: \`22\`
+  }
+}`,...(C=(A=d.parameters)==null?void 0:A.docs)==null?void 0:C.source}}};var R,z,D;g.parameters={...g.parameters,docs:{...(R=g.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div class="text-3xl">
+        Text
+        <Badge v-bind="args">\${args.default}</Badge>
+      </div>
+    \`
+  }),
+  args: {
+    class: 'bg-blue-600',
+    align: 'top',
+    default: \`app v1.8.7\`
+  }
+}`,...(D=(z=g.parameters)==null?void 0:z.docs)==null?void 0:D.source}}};var W,E,U;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div class="text-3xl">
+        Text
+        <Badge v-bind="args">\${args.default}</Badge>
+      </div>
+    \`
+  }),
+  args: {
+    class: 'bg-orange-600',
+    transparent: true,
+    default: \`app v1.8.7\`
+  }
+}`,...(U=(E=u.parameters)==null?void 0:E.docs)==null?void 0:U.source}}};var j,q,G;c.parameters={...c.parameters,docs:{...(j=c.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Badge v-bind="args">\${args.default}</Badge>
+    \`
+  }),
+  args: {
+    class: 'text-blue-600',
+    outline: true,
+    default: \`Outline\`
+  }
+}`,...(G=(q=c.parameters)==null?void 0:q.docs)==null?void 0:G.source}}};var H,J,K;l.parameters={...l.parameters,docs:{...(H=l.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Badge v-bind="args">\${args.default}</Badge>
+    \`
+  }),
+  args: {
+    class: 'bg-red-600',
+    rounded: true,
+    default: \`Round\`
+  }
+}`,...(K=(J=l.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var L,M,N;p.parameters={...p.parameters,docs:{...(L=p.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Badge,
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div class="flex items-center gap-2">
+        Status
+        <Badge v-bind="args">\${args.default}</Badge>
+      </div>
+    \`
+  }),
+  args: {
+    class: 'bg-green-400',
+    default: \`\`
+  }
+}`,...(N=(M=p.parameters)==null?void 0:M.docs)==null?void 0:N.source}}};const ae=["Basic","BasisCustomColors","BasisWithIcon","Floating","FloatingIndicators","Transparent","Aligned","Transparant","OutlineDesign","Rounded","Indicators"];export{g as Aligned,a as Basic,t as BasisCustomColors,r as BasisWithIcon,s as Floating,o as FloatingIndicators,p as Indicators,c as OutlineDesign,l as Rounded,u as Transparant,d as Transparent,ae as __namedExportsOrder,ne as default};
---
+++ docs/assets/Button.stories-C44ZiMda.js
@@ -0,0 +1,405 @@
+import{_ as t}from"./Button-Bd1hwruy.js";import{I as e}from"./Icon-BzsAYEaQ.js";import"./iframe-Dih_2Swd.js";import"./ripple-CsoxYJ_M.js";import"./_plugin-vue_export-helper-CqjgSE-H.js";import"./Primitive-CB9pkJf-.js";const yn={title:"UI/Button",component:t,tags:["autodocs"]},r={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",default:"Standard"}},s={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-blue-500 text-white",default:"Standard"}},a={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-green-400 text-white",default:'<Icon name="shopping_cart" filled size="sm" /> Standard'}},o={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-yellow-500 text-white",round:!0,default:'<Icon name="shopping_cart" filled size="sm" />'}},u={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-purple-500 text-white",rounded:!0,default:"Rounded Button"}},d={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-orange-500 text-white",square:!0,default:'<Icon name="shopping_cart" filled size="sm" />'}},l={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",flat:!0,default:"Flat Button"}},c={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",outline:!0,default:"Outline Button"}},p={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",outline:!0,rounded:!0,default:"Outline rounded Button"}},g={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-cyan-500 text-white",push:!0,default:"Push Button"}},m={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",unelevated:!0,default:"Unelevated Button"}},i={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",dense:!0,default:"Dense Button"}},B={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"",noCaps:!0,default:"NoCaps Button"}},f={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-green-500",glossy:!0,default:"Glossy Button"}},b={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <div class="flex flex-row items-center gap-2">
+        <Button v-bind="args">${n.default}</Button>
+        <Button v-bind="args" size="sm">${n.default}</Button>
+        <Button v-bind="args" size="md">${n.default}</Button>
+        <Button v-bind="args" size="lg">${n.default}</Button>
+        <Button v-bind="args" size="xl">${n.default}</Button>
+      </div>
+    `}),args:{class:"bg-lime-400 text-black",size:"xs",default:'<Icon name="search" /> Button'}},v={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-lime-400 text-black text-6xl",size:"xs",default:'<Icon name="search" /> Button'}},$={render:n=>({components:{Button:t},setup(){return{args:n}},template:`
+      <Button v-bind="args">${n.default}</Button>
+    `}),args:{class:"bg-red-500",disable:!0,default:"Disable Button"}},x={render:n=>({components:{Button:t,Icon:e},setup(){return{args:n}},template:`
+      <div class="flex flex-row flex-wrap items-center gap-2">
+        <Button v-bind="args">${n.default}</Button>
+        <Button v-bind="args" align='left'>${n.default}</Button>
+        <Button v-bind="args" align='between'>${n.default}</Button>
+        <Button v-bind="args" align='around'>${n.default}</Button>
+      </div>
+    `}),args:{class:"bg-pink-500 w-56",align:"right",default:'<Icon name="bolt" /> Alignment Button'}};var h,S,w;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    default: \`Standard\`
+  }
+}`,...(w=(S=r.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};var I,z,C;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-blue-500 text-white',
+    default: \`Standard\`
+  }
+}`,...(C=(z=s.parameters)==null?void 0:z.docs)==null?void 0:C.source}}};var _,y,O;a.parameters={...a.parameters,docs:{...(_=a.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-green-400 text-white',
+    default: \`<Icon name="shopping_cart" filled size="sm" /> Standard\`
+  }
+}`,...(O=(y=a.parameters)==null?void 0:y.docs)==null?void 0:O.source}}};var D,R,k;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-yellow-500 text-white',
+    round: true,
+    default: \`<Icon name="shopping_cart" filled size="sm" />\`
+  }
+}`,...(k=(R=o.parameters)==null?void 0:R.docs)==null?void 0:k.source}}};var U,q,A;u.parameters={...u.parameters,docs:{...(U=u.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-purple-500 text-white',
+    rounded: true,
+    default: \`Rounded Button\`
+  }
+}`,...(A=(q=u.parameters)==null?void 0:q.docs)==null?void 0:A.source}}};var F,G,N;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-orange-500 text-white',
+    square: true,
+    default: \`<Icon name="shopping_cart" filled size="sm" />\`
+  }
+}`,...(N=(G=d.parameters)==null?void 0:G.docs)==null?void 0:N.source}}};var P,W,E;l.parameters={...l.parameters,docs:{...(P=l.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    flat: true,
+    default: \`Flat Button\`
+  }
+}`,...(E=(W=l.parameters)==null?void 0:W.docs)==null?void 0:E.source}}};var j,H,J;c.parameters={...c.parameters,docs:{...(j=c.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    outline: true,
+    default: \`Outline Button\`
+  }
+}`,...(J=(H=c.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var K,L,M;p.parameters={...p.parameters,docs:{...(K=p.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    outline: true,
+    rounded: true,
+    default: \`Outline rounded Button\`
+  }
+}`,...(M=(L=p.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};var Q,T,V;g.parameters={...g.parameters,docs:{...(Q=g.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-cyan-500 text-white',
+    push: true,
+    default: \`Push Button\`
+  }
+}`,...(V=(T=g.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};var X,Y,Z;m.parameters={...m.parameters,docs:{...(X=m.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    unelevated: true,
+    default: \`Unelevated Button\`
+  }
+}`,...(Z=(Y=m.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var nn,tn,en;i.parameters={...i.parameters,docs:{...(nn=i.parameters)==null?void 0:nn.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    dense: true,
+    default: \`Dense Button\`
+  }
+}`,...(en=(tn=i.parameters)==null?void 0:tn.docs)==null?void 0:en.source}}};var rn,sn,an;B.parameters={...B.parameters,docs:{...(rn=B.parameters)==null?void 0:rn.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: '',
+    noCaps: true,
+    default: \`NoCaps Button\`
+  }
+}`,...(an=(sn=B.parameters)==null?void 0:sn.docs)==null?void 0:an.source}}};var on,un,dn;f.parameters={...f.parameters,docs:{...(on=f.parameters)==null?void 0:on.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-green-500',
+    glossy: true,
+    default: \`Glossy Button\`
+  }
+}`,...(dn=(un=f.parameters)==null?void 0:un.docs)==null?void 0:dn.source}}};var ln,cn,pn;b.parameters={...b.parameters,docs:{...(ln=b.parameters)==null?void 0:ln.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div class="flex flex-row items-center gap-2">
+        <Button v-bind="args">\${args.default}</Button>
+        <Button v-bind="args" size="sm">\${args.default}</Button>
+        <Button v-bind="args" size="md">\${args.default}</Button>
+        <Button v-bind="args" size="lg">\${args.default}</Button>
+        <Button v-bind="args" size="xl">\${args.default}</Button>
+      </div>
+    \`
+  }),
+  args: {
+    class: 'bg-lime-400 text-black',
+    size: 'xs',
+    default: \`<Icon name="search" /> Button\`
+  }
+}`,...(pn=(cn=b.parameters)==null?void 0:cn.docs)==null?void 0:pn.source}}};var gn,mn,Bn;v.parameters={...v.parameters,docs:{...(gn=v.parameters)==null?void 0:gn.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-lime-400 text-black text-6xl',
+    size: 'xs',
+    default: \`<Icon name="search" /> Button\`
+  }
+}`,...(Bn=(mn=v.parameters)==null?void 0:mn.docs)==null?void 0:Bn.source}}};var fn,bn,vn;$.parameters={...$.parameters,docs:{...(fn=$.parameters)==null?void 0:fn.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <Button v-bind="args">\${args.default}</Button>
+    \`
+  }),
+  args: {
+    class: 'bg-red-500',
+    disable: true,
+    default: \`Disable Button\`
+  }
+}`,...(vn=(bn=$.parameters)==null?void 0:bn.docs)==null?void 0:vn.source}}};var $n,xn,hn;x.parameters={...x.parameters,docs:{...($n=x.parameters)==null?void 0:$n.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      Button,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div class="flex flex-row flex-wrap items-center gap-2">
+        <Button v-bind="args">\${args.default}</Button>
+        <Button v-bind="args" align='left'>\${args.default}</Button>
+        <Button v-bind="args" align='between'>\${args.default}</Button>
+        <Button v-bind="args" align='around'>\${args.default}</Button>
+      </div>
+    \`
+  }),
+  args: {
+    class: 'bg-pink-500 w-56',
+    align: 'right',
+    default: \`<Icon name="bolt" /> Alignment Button\`
+  }
+}`,...(hn=(xn=x.parameters)==null?void 0:xn.docs)==null?void 0:hn.source}}};const On=["Standard","CustomColor","WithIcon","Round","Rounded","Square","Flat","Outline","OutlineRounded","Push","Unelevated","Dense","NoCaps","Glossy","Sizes","CustomSize","Disable","Alignment"];export{x as Alignment,s as CustomColor,v as CustomSize,i as Dense,$ as Disable,l as Flat,f as Glossy,B as NoCaps,c as Outline,p as OutlineRounded,g as Push,o as Round,u as Rounded,b as Sizes,d as Square,r as Standard,m as Unelevated,a as WithIcon,On as __namedExportsOrder,yn as default};
