90b92a2c38507a782df32067a6c6b76e477da9e7
Silinator <me@silvanfux.ch>
2025-07-13 17:53:23 +0200

    Storybook build.

---
+++ docs/assets/ItemLabel.stories-CseNicVp.js
@@ -0,0 +1,69 @@
+import{a as l,b as i,_ as o,c as r}from"./index-JwVsSc1c.js";import"./index-CTWEWApt.js";import"./utils-DheDpAxB.js";import"./iframe-DfjC8tzZ.js";import"./ripple-CsoxYJ_M.js";import"./Primitive-DcUXdM-a.js";const d={title:"UI/List/ItemLabel",component:l,tags:["autodocs"]},t={render:e=>({components:{List:r,Item:o,ItemSelection:i,ItemLabel:l},setup(){return{args:e}},template:`
+      <List bordered separator rounded class="text-primary">
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">${e.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">${e.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">${e.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+      </List>
+    `}),args:{overline:!1,caption:!1,header:!1,class:"text-primary",default:`
+      <ItemLabel>Item</ItemLabel>
+    `}};var a,n,m;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      List,
+      Item,
+      ItemSelection,
+      ItemLabel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <List bordered separator rounded class="text-primary">
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">\${args.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">\${args.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+        <Item>
+          <ItemSelection>
+            <ItemLabel v-bind="args">\${args.default}</ItemLabel>
+            <ItemLabel caption>Caption</ItemLabel>
+          </ItemSelection>
+        </item>
+      </List>
+    \`
+  }),
+  args: {
+    overline: false,
+    caption: false,
+    header: false,
+    class: 'text-primary',
+    default: \`
+      <ItemLabel>Item</ItemLabel>
+    \`
+  }
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const f=["Basic"];export{t as Basic,f as __namedExportsOrder,d as default};
